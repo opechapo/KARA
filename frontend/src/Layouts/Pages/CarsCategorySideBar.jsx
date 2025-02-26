643737fc2a24a7c0a123a7b3ee73bdd96139c85d
opechapo <abolajiopeyemi000@gmail.com>
2025-02-26 18:20:08 +0100

    motorcycleAndTricycle

--- frontend/src/Layouts/Pages/CarsCategorySideBar.jsx
+++ frontend/src/Layouts/Pages/CarsCategorySideBar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const vehicleCategories = [
-  "Cars",
-  "Buses & Micro Buses",
-  "Motorcycles & Tricycles",
-  "Trucks & Trailers",
-  "Others",
+  { name: "Cars", link: "/cars" },
+  { name: "Buses & Micro Buses", link: "/bus&minibus" },
+  { name: "Motorcycles & Tricycles", link: "/motorcycleandtricycle" },
+  { name: "Trucks & Trailers", link: "#" },
+  { name: "Others", link: "#" },
 ];
 
 const CarsCategorySideBar = () => {
   return (
     <div className="w-64 bg-white shadow-lg rounded-lg p-4">
-
       {/* Categories Header */}
       <h2 className="text-xl font-bold text-white bg-purple-900 p-2 rounded-md text-center ">
         Categories
@@ -24,13 +24,14 @@ const CarsCategorySideBar = () => {
       {/* Category List */}
       <ul className="mt-2 space-y-2">
         {vehicleCategories.map((category, index) => (
-          <li
-            key={index}
-            className="flex justify-between items-center  p-3 rounded-md cursor-pointer hover:bg-gray-100 transition"
-            onClick={() => alert(`Clicked on ${category}`)} // Replace with navigation logic
-          >
-            <span className="text-gray-700 font-medium">{category}</span>
-            <ChevronRight size={20} className="text-gray-500" />
+          <li key={index}>
+            <Link
+              to={category.link}
+              className="flex justify-between items-center p-3 rounded-md cursor-pointer hover:bg-gray-100 transition"
+            >
+              <span className="text-gray-700 font-medium">{category.name}</span>
+              <ChevronRight size={20} className="text-gray-500" />
+            </Link>
           </li>
         ))}
       </ul>
